e934c2648c5b5e442cc17c329d848e3b99285bd8
Wayne <18765312268@gmail.com>
2025-03-07 18:16:11 +0800

    Update server port to 80

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // 中间件
 app.use(cors());
