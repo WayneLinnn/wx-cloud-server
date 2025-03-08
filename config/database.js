3b790ea0c0c64c7c0150076f3e024ba81dd75791
Wayne <18765312268@gmail.com>
2025-03-09 00:38:46 +0800

    Update database configuration with correct instance information and charset settings

--- config/database.js
+++ config/database.js
@@ -7,10 +7,11 @@ dotenv.config({ path: path.join(__dirname, "../.env") });
 
 // 创建连接池配置
 const dbConfig = {
-  host: "10.41.111.100", // 内网地址
+  host: "10.41.111.100", // cynosdbmysql-95n2miaw 内网地址
   port: 3306, // 端口号
   user: "root", // root账号
   password: "Linfeng19960110", // root密码
+  charset: "utf8", // 设置字符集
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -21,6 +22,8 @@ console.log("Database Config:", {
   host: dbConfig.host,
   port: dbConfig.port,
   user: dbConfig.user,
+  charset: dbConfig.charset,
+  instance: "cynosdbmysql-95n2miaw",
 });
 
 const pool = mysql.createPool(dbConfig);
@@ -34,7 +37,9 @@ async function initializeDatabase() {
 
     // 创建数据库（如果不存在）
     console.log("尝试创建数据库...");
-    await connection.query("CREATE DATABASE IF NOT EXISTS bunblebee");
+    await connection.query(
+      "CREATE DATABASE IF NOT EXISTS bunblebee CHARACTER SET utf8 COLLATE utf8_general_ci"
+    );
     console.log("数据库 bunblebee 创建成功或已存在");
 
     // 切换到新创建的数据库
